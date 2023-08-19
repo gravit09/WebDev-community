401756514b888075a63831ed6645ddd017b0ecf0
Gravit09 <garvitchoudhary09@gmail.com>
2023-08-20 03:19:51 +0530

    pre

--- myapp/src/Body.jsx
+++ myapp/src/Body.jsx
@@ -1,11 +1,26 @@
-import React from "react";
-import collabrate from "./assets/collabrate.png";
+import { useState, useEffect } from "react";
+
+import collabrate from "./assets/collab.png";
 import inovate from "./assets/innovate.png";
 import together from "./assets/together.webp";
 import coder from "./assets/coder.png";
 import Join from "./Join";
 import "./index.css";
 function Body() {
+  const [isMobileView, setIsMobileView] = useState(false);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsMobileView(window.innerWidth <= 698);
+    };
+
+    window.addEventListener("resize", handleResize);
+    handleResize();
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
   return (
     <div className="motto">
       <div className="container">
@@ -14,7 +29,18 @@ function Body() {
           <div className="col-lg-6 order-lg-1 ">
             <img className="collab-img" src={collabrate} alt="collabrate-img" />
           </div>
-          <div className="col-lg-6 order-lg-2 ">
+          <div
+            className={`col-lg-6 order-lg-2 ${isMobileView ? "" : "hidden"}`}
+          >
+            <p className="item-p">
+              Web developers collaborate and share knowledge in our dynamic
+              online community. Together, we foster growth, innovation, and
+              success.
+            </p>
+          </div>
+          <div
+            className={`col-lg-6 order-lg-1 ${isMobileView ? "hidden" : ""}`}
+          >
             <p className="item-p">
               Amidst our dynamic digital community, web developers harmoniously
               unite, nurturing collaboration and knowledge exchange. Together,
@@ -28,13 +54,33 @@ function Body() {
           </div>
         </div>
       </div>
+
       <div className="container">
         <div className="row">
           <h1 className="headings">Innovate, Iterate, Elevate</h1>
           <div className="col-lg-6 order-lg-2">
-            <img src={inovate} className="right-img" alt="innovate-img" />
+            {" "}
+            {/* Image column */}
+            <img src={inovate} className="right-img" alt="collabrate-img" />
           </div>
-          <div className="col-lg-6 order-lg-1 animate-from-right">
+          <div
+            className={`col-lg-6 order-lg-1 ${isMobileView ? "" : "hidden"}`}
+          >
+            {" "}
+            {/* Text column */}
+            <p className="item-p">
+              In our online hub, we fuel innovation and growth. Members refine
+              projects, propelling us to excellence. Our relentless progress
+              defines us, celebrating achievements in the journey.
+            </p>
+          </div>
+          <div
+            className={`col-lg-6 order-lg-1 ${
+              isMobileView ? "hidden" : "animate-from-left"
+            }`}
+          >
+            {" "}
+            {/* Text column */}
             <p className="item-p">
               Within our dynamic digital enclave, we ignite a perpetual cycle of
               innovation and refinement. Here, members ardently iterate on
@@ -47,13 +93,25 @@ function Body() {
           </div>
         </div>
       </div>
+
       <div className="container">
         <div className="row">
           <h1 className="headings">Hack Together, Learn Forever</h1>
           <div className="col-lg-6 order-lg-1 ">
-            <img className="together-img" src={together} alt="collabrate-img" />
+            <img className="tog-img" src={together} alt="collabrate-img" />
           </div>
-          <div className="col-lg-6 order-lg-2 animate-from-right">
+          <div
+            className={`col-lg-6 order-lg-2 ${isMobileView ? "" : "hidden"}`}
+          >
+            <p className="item-p">
+              Our mission: collaborative problem-solving. Diverse perspectives
+              tackle challenges, fostering lifelong learning. Our ethos fuels
+              innovation and achievements.
+            </p>
+          </div>
+          <div
+            className={`col-lg-6 order-lg-1 ${isMobileView ? "hidden" : ""}`}
+          >
             <p className="item-p">
               At the heart of our mission lies the advocacy for collaborative
               problem-solving. Here, our members synergize their efforts to hack
@@ -72,9 +130,28 @@ function Body() {
         <div className="row">
           <h1 className="headings">From Novice to Ninja</h1>
           <div className="col-lg-6 order-lg-2">
-            <img src={coder} className="right-img" alt="innovate-img" />
+            {" "}
+            {/* Image column */}
+            <img src={coder} className="right-img" alt="collabrate-img" />
           </div>
-          <div className="col-lg-6 order-lg-1 animate-from-left">
+          <div
+            className={`col-lg-6 order-lg-1 ${isMobileView ? "" : "hidden"}`}
+          >
+            {" "}
+            {/* Text column */}
+            <p className="item-p">
+              Empowering newcomers to excel in web development. Mentors guide
+              with expertise, illuminating the path. Together, we foster growth
+              through mentorship and collaboration.
+            </p>
+          </div>
+          <div
+            className={`col-lg-6 order-lg-1 ${
+              isMobileView ? "hidden" : "animate-from-left"
+            }`}
+          >
+            {" "}
+            {/* Text column */}
             <p className="item-p">
               We take pride in cultivating a nurturing environment where
               newcomers are embraced and empowered on their quest to evolve into
@@ -87,6 +164,7 @@ function Body() {
           </div>
         </div>
       </div>
+
       <h1>Join us</h1>
       <Join />
     </div>
