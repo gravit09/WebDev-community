46e070079232b4c5809b3cd33dbdc377002d2a1d
Gravit09 <garvitchoudhary09@gmail.com>
2023-08-20 00:07:50 +0530

    footer-icon

--- myapp/src/Join.jsx
+++ myapp/src/Join.jsx
@@ -7,50 +7,12 @@ import insta from "./assets/insta.png";
 function Join() {
   return (
     <div>
-      <div className="courses-container">
-        <aside className="profile-card dis">
-          <header>
-            <a href="https://tutsplus.com">
-              <img className="about" src={discord} />
-            </a>
-            <h1>Join our Discord</h1>
-          </header>
-          <div className="profile-bio ">
-            <p>
-              Join community of developers and collobrate with other developers
-            </p>
-          </div>
-        </aside>
-        <aside className="profile-card insta dis">
-          <header>
-            <a href="https://tutsplus.com ">
-              <img className="about" src={insta} />
-            </a>
-            <h1>Code step by step</h1>
-          </header>
-          <div className="profile-bio">
-            <p>
-              Terence is an avid reader of science fiction, especially anything
-              to do with aliens and tweed jackets. Most weekends, he can be
-              found cataloguing his collection of rodent skeletons.
-            </p>
-          </div>
-        </aside>
-        <aside className="profile-card dis">
-          <header>
-            <a href="https://tutsplus.com">
-              <img className="about" src={telegran} />
-            </a>
-            <h1>Code step by step</h1>
-          </header>
-          <div className="profile-bio">
-            <p>
-              Terence is an avid reader of science fiction, especially anything
-              to do with aliens and tweed jackets. Most weekends, he can be
-              found cataloguing his collection of rodent skeletons.
-            </p>
-          </div>
-        </aside>
+      <div className="bottom-container">
+        <div className="social-icons">
+          <img className="social-icon discord" src={discord} />
+          <img className="social-icon telegram" src={telegran} />
+          <img className="social-icon insta" src={insta} />
+        </div>
       </div>
       <p className="footer">
         © Copyrights by We are devs . All rights reserved.
